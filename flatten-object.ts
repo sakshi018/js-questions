bc3dccac9964c95c773b98af4c45edd43955c6a3
Sakshi Agarwal <sakshi018@users.noreply.github.com>
2021-07-21 23:49:16 +0530

    Modified flatten

--- flatten-object.ts
+++ flatten-object.ts
@@ -12,3 +12,14 @@ function flattenObject(obj: Object) {
     }
     return result;
 }
+
+let magic = (obj, parent, finalObj) => {
+    for(let key in obj){
+      if(typeof obj[key] === "object"){
+        magic(obj[key], parent + "_" + key, finalObj);
+      }
+      else {
+        finalObj[parent + "_" + key] = obj[key];
+      }
+    }
+  }
