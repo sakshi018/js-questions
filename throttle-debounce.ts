255c1e30ff7fd42515bd52688f574478d0c36b6f
Sakshi Agarwal <sakshi018@users.noreply.github.com>
2021-08-13 18:47:11 +0530

    added array denounce

--- throttle-debounce.ts
+++ throttle-debounce.ts
@@ -17,3 +17,18 @@ function deboucne(callback, delay = 300) {
         timer = setTimeout(() => callback.apply(this, args), delay);
     };
 }
+
+// throttle array 
+
+const arr = [1,2,3,4];
+
+const myThrottleFunc = (arr, i) => {
+    const throttleTime = 1000;
+
+    if(i<arr.length) {
+        console.log(arr[i]);
+        i++;
+        setTimeout(myThrottleFunc, throttleTime, arr, i);
+    }
+};
+myThrottleFunc(arr, 0);
