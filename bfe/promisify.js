599912e16756ea1a879589a709002f85e0177306
Sakshi Agarwal <sakshi018@users.noreply.github.com>
2021-09-20 13:45:42 +0530

    Add files via upload

---
+++ bfe/promisify.js
@@ -0,0 +1,13 @@
+function promisify(func) {
+    return function (...args) {
+      return new Promise((resolve, reject) => {
+        func.call(this, ...args, (error, data) => {
+          if (error) {
+            reject(error)
+          } else {
+            resolve(data)
+          }
+        })
+      })
+    }
+  }
